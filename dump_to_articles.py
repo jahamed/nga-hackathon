e3dda992b2ac0f875aa477f698a9387f079256ed
Javed <jahamed@avvo.com>
2017-05-20 14:41:21 -0700

    working article post

--- dump_to_articles.py
+++ dump_to_articles.py
@@ -1,21 +1,23 @@
 import xml.etree.ElementTree as ET
 import json
 
-tree = ET.parse('sample_rss_feeds/sample_rss_feed_1.xml')
-root = tree.getroot()
+def XMLParser():
+    tree = ET.parse('sample_rss_feeds/sample_rss_feed_1.xml')
+    root = tree.getroot()
 
-items = []
-for article in root.iter('item'):
-    title = article.find('title')
-    link = article.find('link')
-    description = article.find('description')
-    item = {}
-    item[title.tag] = title.text
-    item[link.tag] = link.text
-    item[description.tag] = description.text
-    items.append(item)
+    items = []
+    for article in root.iter('item'):
+        item = {}
+        item['title'] = article.find('title').text
+        item['source'] = article.find('link').text
+        item['body'] = article.find('description').text
+        items.append(item)
 
-articles = {}
-articles['articles'] = items
-json_data = json.dumps(articles)
-print(json_data)
+    articles = {}
+    articles['articles'] = items
+    json_data = json.dumps(articles)
+    # print(json_data)
+    return articles
+
+if __name__ == '__main__':
+    print("in main")
--- request_test.py
+++ request_test.py
@@ -1,4 +1,6 @@
 import requests
+import json
+from dump_to_articles import XMLParser
 
 payload = {
     'articles' : [
@@ -10,7 +12,7 @@ payload = {
             "tags": ["tag1", "blah"]
         },
         {
-            "title": "another random title",
+            "title": "UPDATED TITLE",
             "body": "Different content",
             "source": "www.cnn.com",
             "date": "5/20/2019",
@@ -19,7 +21,11 @@ payload = {
     ]
 }
 
-# r = requests.post('http://ec2-13-58-7-84.us-east-2.compute.amazonaws.com:8080/raw-articles', data=payload)
-r = requests.get('http://ec2-13-58-7-84.us-east-2.compute.amazonaws.com:8080/articles')
-print (r.json())
-print (r.status_code)
+payload = XMLParser()
+print(payload)
+
+r = requests.post('http://ec2-13-58-7-84.us-east-2.compute.amazonaws.com:8080/articles', json=payload)
+# r = requests.get('http://ec2-13-58-7-84.us-east-2.compute.amazonaws.com:8080/articles')
+# print (r.json())
+# print (r.status_code)
+print(r.text)
