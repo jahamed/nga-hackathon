c051c3a104d834b2f273c0fd3fdba017cc8042fa
Carson Stauffer <carsonstauffer@gmail.com>
2017-05-20 22:54:43 -0700

    Add scraping for CNN

--- scraper.py
+++ scraper.py
@@ -13,6 +13,18 @@ def parseFoxNews(url):
         body.append(paragraph.getText())
     return body
 
+def parseCNN(url):
+    page = urlopen(url)
+    soup = BeautifulSoup(page, "html.parser")
+
+    section = soup.body.find('section', attrs={'id': 'body-text'})
+    div = section.find('div', attrs={'class': 'l-container'})
+    paragraphs = div.findAll('div', attrs={'class': ['zn-body__paragraph', 'el__leafmedia']})
+    body = []
+    for paragraph in paragraphs:
+        body.append(paragraph.getText())
+    return body
+
 # Doesn't work
 def replaceBadStrings(strings):
     return strings # remove this if you want to edit
@@ -26,5 +38,5 @@ def replaceBadStrings(strings):
 
 if __name__ == '__main__':
     print("In main for scraper.py")
-    # str = parseFoxNews("http://www.foxnews.com/politics/2017/05/20/gop-candidate-running-for-governor-presses-mcauliffe-on-climate-change.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+foxnews%2Fpolitics+%28Internal+-+Politics+-+Text%29")
-
+    # parseFoxNews("http://www.foxnews.com/politics/2017/05/20/gop-candidate-running-for-governor-presses-mcauliffe-on-climate-change.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+foxnews%2Fpolitics+%28Internal+-+Politics+-+Text%29")
+    # parseCNN("http://www.cnn.com/2017/05/20/middleeast/iran-rouhani-election/index.html?utm_source=feedburner&utm_medium=feed&utm_campaign=Feed%3A+rss%2Fcnn_world+%28RSS%3A+CNN+-+World%29")
