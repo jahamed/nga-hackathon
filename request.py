5d592431b788423c2cc5bf5a88a69121ff190224
Javed <jahamed@avvo.com>
2017-05-21 10:07:57 -0700

    changing ec2 urls

--- request.py
+++ request.py
@@ -1,11 +1,17 @@
 import requests
 from parsers import parseRSS
 
-fox_url = "http://feeds.foxnews.com/foxnews/politics"
-payload = parseRSS(fox_url)
+fox_urls = [
+    "http://feeds.foxnews.com/foxnews/national",
+    "http://feeds.foxnews.com/foxnews/world",
+    "http://feeds.foxnews.com/foxnews/politics",
+]
 
-r = requests.post('http://ec2-52-15-229-70.us-east-2.compute.amazonaws.com:8080/articles', json=payload)
-# r = requests.get('http://ec2-13-58-7-84.us-east-2.compute.amazonaws.com:8080/articles')
+for fox_url in fox_urls:
+    payload = parseRSS(fox_url)
+    r = requests.post('http://ec2-52-15-229-70.us-east-2.compute.amazonaws.com:8080/articles', json=payload)
+
+# r = requests.get('http://ec2-52-15-229-70.us-east-2.compute.amazonaws.com:8080/articles')
 
 # print (r.json())
 # print (r.status_code)
