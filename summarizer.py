d6c4f981ce402a2ecf9cc968e4e3997623d0d20c
Javed <jahamed@avvo.com>
2017-05-21 11:59:15 -0700

    more cleanup, comments

--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,9 @@
 from gensim.summarization import summarize, keywords
 
+# Use Gensim (TextRank Algorithm) to generate article summary.
 def generate_summary(text):
     return summarize(text, ratio=0.2)
 
-
+# Generate Tags/Keywords using Gensim.
 def generate_tags(text):
     return keywords(text, lemmatize=True, words=5, scores=False, split=True, pos_filter=['NN'])
