4180f8fb02338fdad3e7bc7b61f93973260c76a1
Javed <jahamed@avvo.com>
2017-05-21 11:40:54 -0700

    more comments, status code for requests

--- request.py
+++ request.py
@@ -8,13 +8,13 @@ fox_urls = [
     'http://feeds.foxnews.com/foxnews/internal/travel/mixed',
 ]
 
+# Upload RSS Feed data to our MongoDB
 for fox_url in fox_urls:
+    print("Uploading this url: ", fox_url)
     payload = parseRSS(fox_url)
     r = requests.post('http://ec2-52-15-229-70.us-east-2.compute.amazonaws.com:8080/articles', json=payload)
+    print("Status code: ", r.status_code)
 
-
+# Get all Feeds in our MongoDB
 # r = requests.get('http://ec2-52-15-229-70.us-east-2.compute.amazonaws.com:8080/articles')
-
-# print (r.json())
-# print (r.status_code)
 # print(r.text)
