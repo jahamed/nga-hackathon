7558284de70388439053ceda1d6f617546a7a7bd
Javed <jahamed@avvo.com>
2017-05-21 09:56:02 -0700

    update request

--- request.py
+++ request.py
@@ -1,10 +1,10 @@
 import requests
 from parsers import parseRSS
 
-payload = parseRSS('sample_rss_feeds/sample_rss_feed_1.xml')
-print(payload)
+fox_url = "http://feeds.foxnews.com/foxnews/politics"
+payload = parseRSS(fox_url)
 
-r = requests.post('http://ec2-13-58-7-84.us-east-2.compute.amazonaws.com:8080/articles', json=payload)
+r = requests.post('http://ec2-52-15-229-70.us-east-2.compute.amazonaws.com:8080/articles', json=payload)
 # r = requests.get('http://ec2-13-58-7-84.us-east-2.compute.amazonaws.com:8080/articles')
 
 # print (r.json())
